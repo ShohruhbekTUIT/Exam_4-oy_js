1b28c453bc5a9d0aa8c71ec96d00b70592246c09
ShohruhbekTUIT <96619217+ShohruhbekTUIT@users.noreply.github.com>
2022-04-15 09:25:13 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -20,12 +20,18 @@ function renderUser(arr,element){
   clonedTemplate.querySelector(".users__name").textContent = e.username;
   clonedTemplate.querySelector(".users__title").textContent = e.name;
   clonedTemplate.querySelector(".users__id").textContent = e.id;
-  clonedTemplate.querySelector(".users__address").textContent = e.address.street+" "+e.address.suite+" "+e.address.city+" "+e.address.zipcode;
+  clonedTemplate.querySelector(".users__address").textContent =e.address.street+" "+e.address.suite+" "+e.address.city+" "+e.address.zipcode;
   clonedTemplate.querySelector(".company__name").textContent = e.company.name;
   clonedTemplate.querySelector(".company__catchPhrase").textContent = e.company.catchPhrase;
   clonedTemplate.querySelector(".company__bs").textContent = e.company.bs;
   clonedTemplate.querySelector(".users__tel").textContent = e.phone;
   clonedTemplate.querySelector(".users__tel").setAttribute("href", `tel:${e.phone}`);
+
+  clonedTemplate.querySelector(".users__loc").textContent = "GeoLocation";
+  clonedTemplate.querySelector(".users__loc").setAttribute("href", `https://www.google.com/maps/place/${e.address.geo.lat},${e.address.geo.lng}`);
+
+  clonedTemplate.querySelector(".users__website").textContent = e.website;
+  clonedTemplate.querySelector(".users__website").setAttribute("href", `https://${e.website}`);
 
   clonedTemplate.querySelector(".users__mail").textContent = e.email;
   clonedTemplate.querySelector(".users__mail").setAttribute("href", `mailto:${e.email}`);
